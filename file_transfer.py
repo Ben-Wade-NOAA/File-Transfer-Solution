ce6e0e2d28a72b6653e706ecc82c5bf60a22034d
Ben Wade <ben.wade@noaa.gov>
2024-05-21 19:15:48 +0000

    #129418 #129824 #129825 #129825 Tested and adjusted code for blobs. Blobs are tested and validated after changes to add locks and timeouts

--- file_transfer.py
+++ file_transfer.py
@@ -11,6 +11,7 @@ from azure.storage.blob import BlobClient, ContainerClient, BlobServiceClient
 from azure.identity import DefaultAzureCredential, AzureCliCredential
 from azure.ai.ml import MLClient
 from azureml.fsspec.spec import AzureMachineLearningFileSystem
+from datetime import datetime
 
 
 
@@ -134,7 +135,8 @@ class FileTransferClient:
     
     #region testing functions            
     #helper function to get blob names and folder structure
-    def __print_blob_names(self):
+    def print_blob_names(self):
+        print(self.__target_blobs)
         if(self.__target_blobs):
             for x in range(0, len(self.__target_blobs)):
                 print(self.__target_blobs[x])
@@ -162,7 +164,7 @@ class FileTransferClient:
         self.__used_disk /= self.__to_gb
         self.__free_disk /= self.__to_gb
         #helper
-        print(self.__free_disk)
+        print("Free Disk: "+str(self.__free_disk))
     #gets available ram from the OS on the local compute
     #here's a staged change
     
@@ -179,7 +181,7 @@ class FileTransferClient:
         self.__available_mem = memory_stats[1]/self.__to_gb
         self.__used_mem = memory_stats[3]/self.__to_gb
         self.__free_mem = memory_stats[4]/self.__to_gb
-        print(self.__free_mem)
+        print('Free Memory: '+str(self.__free_mem))
         
     #reads the size of the items to be downloaded from the cloud
     def __get_container_size(self)->float:
@@ -206,6 +208,7 @@ class FileTransferClient:
     def __get_target_blob_list(self, key:str = None):
         if self.__container_client:
             container_client = self.__container_client
+            print(container_client.list_blob_names())
             try:
                 for name in container_client.list_blob_names():
                     if (key in name) and not ('.aml' in name):
@@ -236,6 +239,7 @@ class FileTransferClient:
 
     #takes the folder structure of the target blobs and copies it to the local compute cluster    
     def __copy_folder_structure(self):
+        print('copy folder structure')
         if len(self.__target_blobs)>0:
             working_list = self.__target_blobs
             num_items = len(working_list)
@@ -254,18 +258,32 @@ class FileTransferClient:
             if file[0]=='.':
                 file_list.remove(file)
         return file_list
+    
+    def __change_upload_file_name(self, upload_file:str)->str:
+        print("WARNING: This tool cannot overwrite any existing data in the cloud.")
+        print("The filename has been updated with the current time to avoid data loss in the cloud")
+        now = datetime.now().strftime('%Y-%m-%d-%H-%M-%S')
+        upload_file = upload_file.split('.')
+        extension = upload_file[-1]
+        filename = upload_file[0]+now
+        upload_file = filename+'.'+extension
+        print("New File name: "+upload_file)
+        return upload_file
     #endregion
     
     #region transfer functions
-    
+    def print_storage_type(self):
+        print(self.__dstore_type)
     #these two functions call the correct upload and download functions
     #based on the storage account type
     def get_cloud_folder(self):
-        if self.__blob_or_file == 'blob':
+        if self.__dstore_type == 'blob':
+            print('blob-transfer function')
             self.__transfer_from_blob_to_compute()
-        elif self.__blob_or_file == 'file':
+        elif self.__dstore_type == 'file':
             self.__transfer_from_file_to_compute()
         else:
+            print('how did you get here get_cloud_folder')
             self.__how_did_you_get_here()
     
     def put_local_folder(self, source_folder:str = None, destination_folder:str = None):
@@ -279,11 +297,11 @@ class FileTransferClient:
 
         if(source_folder ==None):
             print("No source folder found, Using folder from Object inantiation")
-            source_folder = self.__local_folder_path
+            source_folder = self.__local_folder_path+'/'+self.__cloud_folder_path
         
-        if self.__blob_or_file == 'blob':
+        if self.__dstore_type == 'blob':
             self.__upload_folder_to_blob(source_folder= source_folder, destination_folder= destination_folder)
-        elif self.__blob_or_file=='file':
+        elif self.__dstore_type=='file':
             self.__upload_folder_to_file(source_folder = source_folder, destination_folder = destination_folder)
         else:
             self.__how_did_you_get_here()
@@ -298,28 +316,33 @@ class FileTransferClient:
         The folder structure will be preserved, but currently cannot copy files from a folders that do not share the same parent directory."""
         blob_service_client = self.__blob_service_client
         container_client = blob_service_client.get_container_client(self.__container_name)
-               
+        print('__transfer from blob to compute')       
         self.__get_available_disk()
         self.__get_available_memory()
         
         if(len(self.__target_blobs)!=0)and(self.__free_disk>self.__container_size):#check to see if there's enough room on the local compute
             try:
                 #timeouts are limited to 2 minutes per mb max, so this sets the timeouts to max.
-                timeout = (self.__container_size/1000.0)*119
-                #a lease is blob talk for a lock. nobody can edit the files while its open
-                lease = container_client.aquire_lease(lease_duration = timeout)
+                timeout = 30.0#(self.__container_size/1000.0)*119
+                
                 self.__copy_folder_structure()#copies to the folder structure
                 for blob_name in self.__target_blobs:#loops through each blob
+
+                    blob_client = container_client.get_blob_client(blob = blob_name)
+                    print(blob_client.exists())
                     cleaned_blob_name = blob_name.split('/')#probably doesn't need to happen, but splits up by folders
+
                     try:
+                        lease = blob_client.acquire_lease()
                         #download each blob 1x1
-                        with open(file = os.path.join(self.__local_folder_path, *cleaned_blob_name), mode = 'wb') as download_file:
-                            download_file.write(container_client.download_blob(blob_name).readall())
+                        target_path = os.path.join(self.__local_folder_path, *cleaned_blob_name)
+                        with open(file = target_path, mode = 'wb') as download_file:
+                            download_file.write(blob_client.download_blob().readall())
+                        lease.break_lease()
                     except Exception as e:
-                        print(e)
+                        lease.break_lease()
                 #cleanup the lease and container client
-                lease.break_lease()
-                container_client.close()
+                    container_client.close()
             except Exception as e:
                 print("Something went wrong with the file transfer from {} to {}. Please check the logs and try again".format(self.__container_name, self.__local_folder_path))
         else:
@@ -332,6 +355,7 @@ class FileTransferClient:
         container_client = local_blob_client.get_container_client(container=self.__container_name)
         #get files in whatever directory you're trying to upload
         local_file_list = os.listdir(source_folder)
+        print("before processing: ", local_file_list)
         #strip out the files that were downloaded to begin with
         local_file_list = [file_name for file_name in local_file_list if (file_name not in self.__target_blobs) and not ('.amlignore' in file_name)]  #I hate this line of code but it's otherwise really inefficient      
         #upload the files that are left using the container client
@@ -339,21 +363,34 @@ class FileTransferClient:
         print(local_file_list)
         print(source_folder)
         print(self.__local_folder_path)
-       
+
         try:
             #put a lock on the containers where this stuff is going
-            lease = container_client.lease(lease_timeout = -1)  
+        #    lease = container_client.lease(lease_timeout = -1)  
             for upload_file in local_file_list:
                 
+                blob_client = container_client.get_blob_client(blob = os.path.join(destination_folder, upload_file))
+                is_old_blob = blob_client.exists()
+                print("is old blob: "+str(is_old_blob))
+                if is_old_blob:
+                    print("A blob with the provided name exists. The name is being changed to prevent data loss")
+                    updated_upload_file = self.__change_upload_file_name(upload_file=upload_file)
+                else:
+                    print("Creating blob for {}".format(upload_file))
+                    updated_upload_file = upload_file
+                
                 with open(file = os.path.join(source_folder, upload_file), mode = 'rb') as data:
+                    print(type(data))
                     print(os.path.join(destination_folder, upload_file))
-                    blob_client = container_client.upload_blob(name = os.path.join(destination_folder, upload_file), data = data, overwrite = False)
+                    blob_client = container_client.upload_blob(name = os.path.join(destination_folder, updated_upload_file), data = data, overwrite = False)
             print("Upload Complete, please verify with Azure Storage Explorer")
-            lease.break_lease()
-        except Exception as e:
-            print("an excpetion occurred, probably because another user is uploading to the same location. Please try again")
 
+        except Exception as e:
+            print(e)
+            print("an exception occurred, probably because another user is uploading to the same location. Please try again")
         container_client.close()
+      
+   
     #endregion
 
     #region files
@@ -367,15 +404,16 @@ class FileTransferClient:
 
         if(len(self.__target_files)!=0) and (self.__free_disk>self.__container_size):
             try:
-                self.__azmlfs.start_tranaction()
+                self.__azmlfs.start_transaction()
                 for path in self.__target_files:
                     try:
                         self.__azmlfs.get_file(rpath = path, lpath= self.__local_folder_path)
                     except Exception as e:
                         print(e)
-                self.__azmlfs.close_transaction()
+                self.__azmlfs.end_transaction()
 
             except Exception as e:
+                self.__azmlfs.end_transaction()
                 print("Something went wrong with the file transfer from {} to {}. Please check the logs and try again".format(self.__container_name, self.__local_folder_path))
         else:
             print("No files found containing the characters: {} or the size of the download exceeds the available disk on the compute target".format(self.__cloud_folder_path))
@@ -411,7 +449,7 @@ class FileTransferClient:
             print("Upload Complete, please verify with Azure Storage Explorer")
             
         except Exception as e:
-            print("an excpetion occurred, probably because another user is uploading to the same location. Please try again")
+            print("an exception occurred, probably because another user is uploading to the same location. Please try again")
             self.__azmlfs.end_transaction()
     #endregion
     #endregion
